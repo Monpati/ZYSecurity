7844d39c6690db78e84b4676ca9b8c1df3740e51
Monpati <59530377+Monpati@users.noreply.github.com>
2024-10-15 18:04:16 +0800

    Delete 德迅 directory

--- 德迅/rest/config/dexun/dexunCert.go
+++
@@ -1,9 +0,0 @@
-package dexun
-
-import "Dexun/model/Dexun"
-
-func CertInfo(gname, gnum string) (string, string, string, string) {
-	var d Dexun.DeXunBody
-	d.GetPersonCertDetails(gname, gnum)
-	return d.SmrzBirthday, d.SmrzSex, d.SmrzGnum, d.SmrzCity
-}
